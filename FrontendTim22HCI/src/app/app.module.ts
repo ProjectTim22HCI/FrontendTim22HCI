b68dc7dac59f402481acf0a5b1edbde7aaf3e8d3
Filip Vuksan <filipvuksan.iphone@gmail.com>
2023-04-06 18:26:07 +0200

    bootstrap init

--- FrontendTim22HCI/src/app/app.module.ts
+++ FrontendTim22HCI/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HomeContentComponent } from './Components/home-content/home-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeContentComponent
   ],
   imports: [
     BrowserModule,
